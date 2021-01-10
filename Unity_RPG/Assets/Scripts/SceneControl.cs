45ec9860b56270ca5caff52e911a6d4466e9ab10
jhjjkj <75553515+jhjjkj@users.noreply.github.com>
2021-01-10 17:16:11 +0800

    設定遊戲

--- Unity_RPG/Assets/Scripts/SceneControl.cs
+++ Unity_RPG/Assets/Scripts/SceneControl.cs
@@ -3,12 +3,18 @@ using UnityEngine.SceneManagement; // 引用 場景管理 API
 
 public class SceneControl : MonoBehaviour
 {
+    /// <summary>
+    /// 開始遊戲
+    /// </summary>
     public void StartGame()
     {
         // 場景管理器.載入場景("場景名稱")
         SceneManager.LoadScene("遊戲場景");
     }
 
+    /// <summary>
+    /// 返回選單
+    /// </summary>
     public void BackToMenu()
     {
         SceneManager.LoadScene("選單");
