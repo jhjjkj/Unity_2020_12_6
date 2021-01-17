3aeb530d1b203967709d1126761213946efdc66b
jhjjkj <75553515+jhjjkj@users.noreply.github.com>
2021-01-17 17:34:02 +0800

    玩家跳躍

---
+++ Unity_RPG/Assets/Scripts/LearnCSharp.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class LearnCSharp : MonoBehaviour
+{
+    public int a = 10;
+    public int b = 3;
+
+    private void Start()
+    {
+        // 比較運算子
+        // 傳回布林值
+        print(a > b);        // true
+        print(a < b);        // false
+        print(a == b);       // 等於 false
+        print(a != b);       // 不等 true
+
+        // 邏輯運算子
+        // 並且 &&
+        // 有一個 F 結果就是 F
+        print(true && true);         // T
+        print(true && false);        // F
+        print(false && true);        // F
+        print(false && false);       // F
+
+        // 或者 ||
+        // 有一個 T 結果就是 T
+        print(true || true);         // T
+        print(true || false);        // T
+        print(false || true);        // T
+        print(false || false);       // F
+
+        print("相反運算子:" + (!true));      // F
+        print("相反運算子:" + (!false));      // T
+    }
+}
--- Unity_RPG/Assets/Scripts/Player.cs
+++ Unity_RPG/Assets/Scripts/Player.cs
@@ -19,6 +19,10 @@ public class Player : MonoBehaviour
     public AudioClip soundFire;
     [Header("血量"), Range(0, 200)]
     public float hp = 100;
+    [Header("地面判定位移")]
+    public Vector3 offset;
+    [Header("地面判定半徑")]
+    public float radius = 0.3f;
 
     private AudioSource aud;
     private Rigidbody2D rig;
@@ -38,12 +42,23 @@ public class Player : MonoBehaviour
 
         // 剛體欄位 = 取得元件<剛體>()
         rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
 
     private void Update()
     {
         GetHorizontal();
         Move();
+        Jump();
+    }
+
+    // 在 Unity 內繪製圖示
+    private void OnDrawGizmos()
+    {
+        // 圖示.顏色 = 顏色
+        Gizmos.color = new Color(1, 0, 0, 0.35f);
+        // 圖示.繪製球體(中心點, 半徑)
+        Gizmos.DrawSphere(transform.position + offset, radius);
     }
 
     #region 方法
@@ -63,6 +78,22 @@ public class Player : MonoBehaviour
     {
         // 剛體.加速度 = 二維(水平 * 速度, 原本加速度的 Y)
         rig.velocity = new Vector2(h * speed, rig.velocity.y);
+
+        // 如果 玩家 按下 D 或者 右鍵 就執行 { 內容 }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // transform 指的與此腳本同一層的 Transform 元件
+            // Rotation 角度在程式是 localEulerAngles
+            transform.localEulerAngles = Vector3.zero;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+
+        // 動畫控制器.設定布林值(參數, 布林值)
+        // 玩家按下左或右時 跑步 h != 0
+        ani.SetBool("跑步開關", h != 0);
     }
 
     /// <summary>
@@ -70,7 +101,25 @@ public class Player : MonoBehaviour
     /// </summary>
     private void Jump()
     {
+        // 如果 在地面上 並且 按下空白鍵 才可以 跳躍
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(new Vector2(0, jump));         // 剛體.添加推力(二維向量)
+            isGrounded = false;                         // 不在地面上
+        }
 
+        // 碰撞物件 = 2D 物理.覆蓋圖形(中心點, 半徑, 圖層) - 1 << 圖層
+        Collider2D hit = Physics2D.OverlapCircle(transform.position + offset, radius, 1 << 8);
+        // 如果 碰到的物件 存在的 就將 是否在地面上 設定為 是
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        // 否則 沒有碰到物件 就將 是否在地面上 設定為 否
+        else
+        {
+            isGrounded = false;                          // 不在地面上
+        }
     }
 
     /// <summary>
